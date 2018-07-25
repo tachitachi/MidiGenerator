5aee20a151a1be5b04b03b322abbe03ad76f5071
Aaron Keech <aaronkeech@gatech.edu>
2018-07-25 00:00:08 -0400

    Prepare data with music box encoding

---
+++ data2.py
@@ -0,0 +1,248 @@
+import mido
+import numpy as np
+import os
+import argparse
+from tqdm import tqdm
+import tensorflow as tf
+
+from collections import defaultdict
+
+def _int64_feature(value):
+	return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+def _bytes_feature(value):
+	return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+def _float_feature(value):
+	return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+class MidiDataset(object):
+	splits = {
+		'train': 'train.tfrecord',
+		'test': 'test.tfrecord',
+	}
+
+	def __init__(self, split, data_dir):
+		self.data_dir = data_dir
+
+		self.processed_dir = os.path.join(self.data_dir, 'processed')
+
+		if not os.path.isfile(os.path.join(self.processed_dir, MidiDataset.splits['train'])) or not os.path.isfile(os.path.join(self.processed_dir, MidiDataset.splits['test'])):
+			all_files = []
+
+			for root, dirs, files in os.walk(data_dir):
+				all_files.extend(list(map(lambda x: os.path.join(root, x), files)))
+
+			# randomly choose 20% for the test set
+			train_idx = sorted(np.random.choice(np.arange(len(all_files)), int(len(all_files) * 0.8), replace=False))
+			test_idx = sorted(list(set(np.arange(len(all_files))) - set(train_idx)))
+
+
+			def process(all_files, idx, output_name):
+				os.makedirs(self.processed_dir, exist_ok=True)
+
+				writer = tf.python_io.TFRecordWriter(os.path.join(self.processed_dir, '%s.tfrecord' % output_name))
+
+				with open(os.path.join(self.processed_dir, '%s.txt' % output_name), 'w') as f:
+					for i in tqdm(idx):
+						file = all_files[i]
+						f.write('%s\n' % os.path.basename(file))
+						data = np.load(file)
+
+						shape = list(data.shape)
+						notes = data.shape[1]
+						length = data.shape[0]
+
+						feature = {
+							'data': _int64_feature(data.reshape([-1])),
+							'notes': _int64_feature([notes]),
+							'length': _int64_feature([length]),
+						}
+
+						example = tf.train.Example(features=tf.train.Features(feature=feature))
+						writer.write(example.SerializeToString())
+
+			process(all_files, train_idx, 'train')
+			process(all_files, test_idx, 'test')
+
+		self.x = self._load(split)
+
+	def _load(self, split):
+    # sess.run(tf.get_collection(tf.GraphKeys.TABLE_INITIALIZERS))
+
+		def _parse_function(example_proto):
+			features = {
+				'data': tf.FixedLenSequenceFeature([], dtype=tf.int64, allow_missing=True),
+				'notes': tf.FixedLenFeature([1], dtype=tf.int64),
+				'length': tf.FixedLenFeature([1], dtype=tf.int64),
+			}
+			parsed_features = tf.parse_single_example(example_proto, features)
+			print(parsed_features)
+
+			#return tf.reshape(parsed_features['data'], parsed_features['shape'])
+			return tf.reshape(parsed_features['data'], [-1, 128])
+
+		dataset = tf.data.TFRecordDataset(os.path.join(self.processed_dir, MidiDataset.splits[split]))
+		dataset = dataset.map(_parse_function)
+
+		dataset = dataset.repeat()
+
+		iterator = dataset.make_initializable_iterator()
+		next_x = iterator.get_next()
+
+		tf.add_to_collection(tf.GraphKeys.TABLE_INITIALIZERS, iterator.initializer)
+
+		return next_x
+						
+				
+class Note(object):
+	note = 0
+	on = False
+	time = 0
+
+
+class MidiParse(object):
+	note_on = 0
+	note_off = 1
+	velocity = 2
+	time = 3
+
+	def __init__(self, data_dir, notes=128, step_size=16):
+		self.data_dir = data_dir
+		self.notes = notes
+		self.step_size = step_size
+
+		self.ticks_per_beat = 480
+		self.tempo = 500000
+
+		self.unit_ticks = int(mido.second2tick(0.01, self.ticks_per_beat, self.tempo))
+		self.ticks_per_second = int(mido.second2tick(1, self.ticks_per_beat, self.tempo))
+
+		if not os.path.isdir(self.data_dir):
+			os.makedirs(self.data_dir)
+
+	def _parse_vec(self, event):
+		shape = self.notes + self.notes + self.velocities + self.times
+		vec = np.zeros(shape)
+
+		if event[0] == MidiParse.note_on:
+			vec[event[1]] = 1
+		if event[0] == MidiParse.note_off:
+			vec[self.notes + event[1]] = 1
+		if event[0] == MidiParse.velocity:
+			vec[self.notes + self.notes + event[1]] = 1
+		if event[0] == MidiParse.time:
+			vec[self.notes + self.notes + self.velocities + event[1]] = 1
+
+		return vec
+
+	def update_timing(self, midi):
+
+		self.ticks_per_beat = midi.ticks_per_beat
+
+		meta_track = midi.tracks[0]
+
+		for msg in meta_track:
+			if hasattr(msg, 'tempo'):
+				self.tempo = msg.tempo
+
+		self.unit_ticks = int(mido.second2tick(0.01, self.ticks_per_beat, self.tempo))
+		self.ticks_per_second = int(mido.second2tick(1, self.ticks_per_beat, self.tempo))
+
+	def ticksToUnits(self, time):
+		ticks_per_unit = self.ticks_per_second / self.step_size
+
+		return np.round(time / ticks_per_unit).astype(np.int32)
+
+	def updateEncoding(self, note, start, end, encoding):
+		start_unit = self.ticksToUnits(start)
+		end_unit = self.ticksToUnits(end)
+		encoding[start_unit:end_unit,note] = 1
+
+	def parse(self, path):
+		midi = mido.MidiFile(path)
+
+		self.update_timing(midi)
+
+		#print('ticks per second: %d' % self.ticks_per_second)
+		ticks_per_unit = self.ticks_per_second / self.step_size
+		#print('ticks per unit: %d' % ticks_per_unit)
+
+		unit = 1 / self.step_size
+
+		song_length = np.ceil(midi.length * self.step_size).astype(np.int32)
+
+		if midi.length < 30:
+			return
+
+		notes_in_use = defaultdict(Note)
+
+		# Create music box encoding
+		encoding = np.zeros((song_length, self.notes), dtype=np.uint8)
+
+		# note_on velocity=0, and note_off are equivalent
+		tracks = mido.merge_tracks(midi.tracks[1:])
+		total_time = 0
+		for msg in tracks:
+			if msg.is_meta or not hasattr(msg, 'note'):
+				continue
+			if msg.type == 'note_on':
+				note = msg.note
+				time = msg.time
+				velocity = msg.velocity
+
+				total_time += time
+
+				if velocity == 0:
+					prev_time = notes_in_use[note].time
+					#print('%d from %d - %d' % (note, self.ticksToUnits(prev_time), self.ticksToUnits(total_time)))
+					self.updateEncoding(note, prev_time, total_time, encoding)
+					
+				else:
+					notes_in_use[note].note = note
+					notes_in_use[note].on = True
+					notes_in_use[note].time = total_time
+
+
+			if msg.type == 'note_off':
+				note = msg.note
+				time = msg.time
+
+				total_time += time
+
+				prev_time = notes_in_use[note].time
+				#print('%d from %d - %d' % (note, self.ticksToUnits(prev_time), self.ticksToUnits(total_time)))
+				self.updateEncoding(note, prev_time, total_time, encoding)
+
+
+		#print(encoding[:,60])
+		#print(np.sum(encoding))
+
+		basename, _ = os.path.splitext(os.path.basename(path))
+		out_path = os.path.join(self.data_dir, basename)
+		np.save(out_path, encoding)
+
+def create_batch(tensors, batch_size=32, shuffle=False, queue_size=10000, min_queue_size=5000, num_threads=1):
+    # Must initialize tf.GraphKeys.QUEUE_RUNNERS
+    # tf.train.start_queue_runners(sess=sess)
+    if shuffle:
+        return tf.train.shuffle_batch(tensors, batch_size=batch_size, capacity=queue_size, min_after_dequeue=min_queue_size, num_threads=num_threads)
+    else:
+        return tf.train.batch(tensors, batch_size=batch_size, capacity=queue_size, num_threads=num_threads)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--data_dir', type=str, default='data')
+	parser.add_argument('--output_dir', type=str, default='data/parsed')
+
+	args = parser.parse_args()
+
+	parser = MidiParse(args.output_dir)
+
+	for root, dirs, files in os.walk(args.data_dir):
+		for file in tqdm(files):
+			if file.endswith('.mid') or file.endswith('.midi'):
+				#parser.parse(os.path.join(root, file))
+				try:
+					parser.parse(os.path.join(root, file))
+				except:
+					print('failed to parse midi file %s' % file)
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import argparse
 import tensorflow as tf
-import data
+import data2
 import numpy as np
 from tqdm import tqdm
 import os
@@ -10,57 +10,58 @@ import model
 def main(args):
 
 	# get datasets
-	dataset = data.MidiDataset(args.split, args.dataset_dir)
+	dataset = data2.MidiDataset(args.split, args.dataset_dir)
 
-	x, y = dataset.x, dataset.y
+	x = dataset.x
 
-	batch_x, batch_y = data.create_batch([x, y], batch_size=args.batch_size)
-
-
-	# build models
-
-	generator = model.SequenceGenerator(dataset.vec_size, scope='model')
-
-	preds = generator(batch_x)
-	words = tf.nn.softmax(preds)
-
-	accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(preds, 1), batch_y), tf.float32))
-	tf.summary.scalar('accuracy', accuracy)
-
-	# create loss functions
-
-	total_loss = tf.losses.sparse_softmax_cross_entropy(batch_y, preds)
-	
-
-	inc_global_step = tf.assign_add(tf.train.get_or_create_global_step(), 1)
-	tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, inc_global_step)
-
-	update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-	with tf.control_dependencies(update_ops):
-		train_tensor = tf.train.AdamOptimizer(args.learning_rate).minimize(total_loss)
-
-		# Set up train op to return loss
-		with tf.control_dependencies([train_tensor]):
-			train_op = tf.identity(total_loss, name='train_op')
+	#batch_x = data2.create_batch([x], batch_size=args.batch_size)
+	batch_x = tf.expand_dims(x, 0)
 
 
-
-
-	# set up logging
-
-	# Gather initial summaries.
-	summaries = set(tf.get_collection(tf.GraphKeys.SUMMARIES))
-
-	# Add summaries for losses.
-	for loss in tf.get_collection(tf.GraphKeys.LOSSES):
-		summaries.add(tf.summary.scalar('losses/%s' % loss.op.name, loss))
-
-	# Add summaries for variables.
-	for variable in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
-		summaries.add(tf.summary.histogram(variable.op.name, variable))
-
-	# Merge all summaries together.
-	summary_op = tf.summary.merge(list(summaries), name='summary_op') if len(summaries) > 0 else tf.no_op() 
+#	# build models
+#
+#	generator = model.SequenceGenerator(dataset.vec_size, scope='model')
+#
+#	preds = generator(batch_x)
+#	words = tf.nn.softmax(preds)
+#
+#	accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(preds, 1), batch_y), tf.float32))
+#	tf.summary.scalar('accuracy', accuracy)
+#
+#	# create loss functions
+#
+#	total_loss = tf.losses.sparse_softmax_cross_entropy(batch_y, preds)
+#	
+#
+#	inc_global_step = tf.assign_add(tf.train.get_or_create_global_step(), 1)
+#	tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, inc_global_step)
+#
+#	update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+#	with tf.control_dependencies(update_ops):
+#		train_tensor = tf.train.AdamOptimizer(args.learning_rate).minimize(total_loss)
+#
+#		# Set up train op to return loss
+#		with tf.control_dependencies([train_tensor]):
+#			train_op = tf.identity(total_loss, name='train_op')
+#
+#
+#
+#
+#	# set up logging
+#
+#	# Gather initial summaries.
+#	summaries = set(tf.get_collection(tf.GraphKeys.SUMMARIES))
+#
+#	# Add summaries for losses.
+#	for loss in tf.get_collection(tf.GraphKeys.LOSSES):
+#		summaries.add(tf.summary.scalar('losses/%s' % loss.op.name, loss))
+#
+#	# Add summaries for variables.
+#	for variable in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
+#		summaries.add(tf.summary.histogram(variable.op.name, variable))
+#
+#	# Merge all summaries together.
+#	summary_op = tf.summary.merge(list(summaries), name='summary_op') if len(summaries) > 0 else tf.no_op() 
 
 
 	# create train loop
@@ -68,7 +69,7 @@ def main(args):
 	if not os.path.isdir(args.output_dir):
 		os.makedirs(args.output_dir)
 
-	saver = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='model'))
+#	saver = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='model'))
 	checkpoint_path = os.path.join(args.output_dir, 'model.ckpt')
 	writer = tf.summary.FileWriter(args.output_dir)
 
@@ -82,6 +83,10 @@ def main(args):
 		last_log_time = 0
 		last_save_time = 0
 		for i in tqdm(range(args.num_batches)):
+
+			print(sess.run(batch_x))
+
+			continue
 
 			if last_log_time < time.time() - args.log_every_n_seconds:
 				last_log_time = time.time()
@@ -103,8 +108,8 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--split', type=str, default='train')
 	parser.add_argument('--dataset_dir', type=str, default='data')
-	parser.add_argument('--batch_size', type=int, default=128)
-	parser.add_argument('--num_batches', type=int, default=100000)
+	parser.add_argument('--batch_size', type=int, default=1)
+	parser.add_argument('--num_batches', type=int, default=1)
 	parser.add_argument('--shuffle', type=bool, default=True)
 	parser.add_argument('--output_dir', type=str, default='output/%d' % int(time.time() * 1000))
 	parser.add_argument('--log_every_n_seconds', type=int, default=30)
